b97be93653493a9aa9a1feef923c603ed4c99f9b
Emory Penney <treadstoneit@gmail.com>
2024-04-17 14:37:54 -0700

    Update coordinator.py

--- custom_components/eufy_security/coordinator.py
+++ custom_components/eufy_security/coordinator.py
@@ -103,7 +103,7 @@ class EufySecurityDataUpdateCoordinator(DataUpdateCoordinator):
 
     def _on_error(self, error):
         """raise notification on frontend when exception happens"""
-        create(f"Connection to Eufy Security add-on is broken, retrying in background!", title="Eufy Security - Error", notification_id="eufy_security_addon_connection_error")
+        create(self.hass, f"Connection to Eufy Security add-on is broken, retrying in background!", title="Eufy Security - Error", notification_id="eufy_security_addon_connection_error")
         self.hass.bus.async_listen_once(DISCONNECTED, self._async_reload)
         self.hass.bus.async_fire(DISCONNECTED, None)
 
