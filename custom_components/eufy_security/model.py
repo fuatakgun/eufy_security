12d8652c59921709a0e8d116230961ad83e725e9
Snuffy2 <Snuffy2@users.noreply.github.com>
2024-05-19 13:56:28 -0400

    Remove deprecated homeassistant.backports.enum

--- custom_components/eufy_security/model.py
+++ custom_components/eufy_security/model.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from enum import Enum, auto
+from enum import auto, Enum, StrEnum
 
-from homeassistant.backports.enum import StrEnum
 from homeassistant.config_entries import ConfigEntry
 
 
